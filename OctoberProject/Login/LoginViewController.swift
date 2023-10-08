710e51cc6833c83844dbb95bc3b74d47ab2c4cfb
Gameover1987 <slnec@yandex.ru>
2023-10-08 15:09:58 +0700

    Добавил SnapKit, добавил вьюконтроллер для логина

---
+++ OctoberProject/Login/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  OctoberProject
+//
+//  Created by Вячеслав on 08.10.2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
