c216077fa1f4aec12a5e2abf5e958c1b0aeffb82
Gameover1987 <slnec@yandex.ru>
2023-10-14 21:04:31 +0700

    SwiftLinter fix

--- OctoberProject/Resources/Colors.swift
+++ OctoberProject/Resources/Colors.swift
@@ -9,17 +9,17 @@ import Foundation
 import UIKit
 
 final class Colors {
-    public static let loginBackgroundColor = UIColorUtils.CreateFromRGB(red: 55, green: 109, blue: 254)
+    public static let loginBackgroundColor = UIColorUtils.createFromRGB(red: 55, green: 109, blue: 254)
     
-    public static let buttonColor = UIColorUtils.CreateFromRGB(red: 29, green: 133, blue: 255)
+    public static let buttonColor = UIColorUtils.createFromRGB(red: 29, green: 133, blue: 255)
     
-    public static let buttonColor2 = UIColorUtils.CreateFromRGB(red: 111, green: 119, blue: 132)
+    public static let buttonColor2 = UIColorUtils.createFromRGB(red: 111, green: 119, blue: 132)
     
-    public static let borderColor = UIColorUtils.CreateFromRGB(red: 233, green: 232, blue: 236)
+    public static let borderColor = UIColorUtils.createFromRGB(red: 233, green: 232, blue: 236)
 }
 
 public class UIColorUtils {
-    public static func CreateFromRGB(red: uint, green: uint, blue: uint) -> UIColor {
+    public static func createFromRGB(red: uint, green: uint, blue: uint) -> UIColor {
         return UIColor(red: CGFloat(red) / 255, green: CGFloat(green) / 255, blue: CGFloat(blue) / 255, alpha: 1)
     }
 }
